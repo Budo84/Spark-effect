0567605a648674d2352f835633100ab84ab6d56c
Budo84 <lamanteafabio@gmail.com>
2026-03-30 18:07:58 +0200

    Add icons to cache for offline use

--- sw.js
+++ sw.js
@@ -4,7 +4,9 @@ const CACHE_NAME = 'spark-editor-v9';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
